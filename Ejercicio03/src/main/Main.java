45b2a2b9d36716e5419f482a6f93c08dfdaf9b15
Javi.LeL <javier212002@gmail.com>
2024-01-21 19:02:17 +0100

    añadiendo el ejercicio 3

---
+++ Ejercicio03/src/main/Main.java
@@ -0,0 +1,57 @@
+package main;
+/*
+ * Dada una cadena s, encuentre la longitud de la subcadena más larga sin caracteres repetidos.
+ * 
+ * Example 1:
+ * Input: abcabcbb
+ * Output: 3
+ * 
+ * Example 2:
+ * Input: bbbbb
+ * Output: 1
+ * 
+ * Example 3:
+ * Imput: pwwkew
+ * Output: 3
+ */
+
+/**
+ * Main
+ * @author javier
+ * @version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(algoritmo("abcabcbb"));
+    }
+    /**
+     * Metodo que alberga el algoritmo solicitado
+     */
+    public static int algoritmo(String s){
+        int aux = 0, resultado = 0;
+        for(int i=1;i<s.length();i++){
+            if (isRepetido(s.substring(aux, i))) {
+                aux=i-1;
+            }else{
+                resultado = resultado<=s.substring(aux, i).length()?s.substring(aux, i).length():resultado;
+                // System.out.println(s.substring(aux, i));
+            }
+        }
+        return resultado;
+    }
+    /**
+     * Metodo que busca que ningun caracter se repita en el String que se le pasa.
+     * @param s
+     * @return
+     */
+    public static boolean isRepetido(String s){
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j < s.length(); j++) {
+                if(s.charAt(i)==s.charAt(j) && i!=j){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
